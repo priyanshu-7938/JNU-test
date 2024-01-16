93d5dc8f60d10e0f002a28bb5a997d8d7b170767
priyanshu_owner <67639480+priyanshu-7938@users.noreply.github.com>
2024-01-16 06:10:11 +0530

    trivial stuff

--- frontend/the_admin_view/src/components/Login.jsx
+++ frontend/the_admin_view/src/components/Login.jsx
@@ -3,6 +3,7 @@ import { useTheContext } from "../context";
 import { useNavigate } from "react-router-dom";
 
 export default function Login(){
+    const [ loginTo, setLoginTo ] = useState(false);// false = admin
     const navigate = useNavigate();
     const [ password, setPassword ] = useState(null);
     const [ username, setUsername ] = useState(null);
@@ -42,7 +43,14 @@ export default function Login(){
             MessageBar("Fill all entries...","info");
             return;
         }
-        const url = "http://localhost:2024/user/login";
+        let url;
+        if(loginTo){
+            url = "http://localhost:2024/user/login";
+        }
+        else{
+            url = "http://localhost:2024/admin/login";
+
+        }
         var data = new URLSearchParams();
         data.append("username", username);
         data.append("password", password);
@@ -72,12 +80,19 @@ export default function Login(){
             // console.error('Error:', error);
         });
     }
+    function toggleButton(){
+        setLoginTo((prev)=> !prev);
+    }
 
 
 
     return (<>
         <div className="font-bold mt-[100px] mx-auto w-[400px] border-2 p-5 text-[#6DA4AA]">
-            <p className="text-6xl py-2">Login</p>
+            <p className="text-6xl py-2 flex items-center">Login 
+                <button
+                class="bg-gray-300 text-gray-700 px-4 h-[50%] rounded-full text-sm text-bold focus:outline-none focus:shadow-outline"
+                onClick={toggleButton}>{loginTo? <>User</>:<>Admin</>}!</button>
+            </p>
             <p className="py-1 font-2xl text-[#647D87]">Username</p>
             <input required type="text" className="focus:outline-none p-2 px-3 h-[50px] border-3 bg-[#FAEF9B] rounded-[20px] w-full text-xl" onChange={handleUsername}/>
             <p className="py-1 font-2xl text-[#647D87]">Password</p>

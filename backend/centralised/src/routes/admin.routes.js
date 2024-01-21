9fc0b50205beb8ade697663faf16de8722bfb90f
soham haldar <sohamhaldar25@gmail.com>
2024-01-21 16:55:41 +0530

    addAdmin

--- backend/centralised/src/routes/admin.routes.js
+++ backend/centralised/src/routes/admin.routes.js
@@ -1,4 +1,4 @@
-import { generateProof,getProofRequests,loginUser } from "../controllers/admin.controller.js";
+import { generateProof,getProofRequests,loginUser, registerAdmin } from "../controllers/admin.controller.js";
 import {Router} from "express";
 
 const router=Router();
@@ -6,5 +6,6 @@ const router=Router();
 router.route("/login").post(loginUser);
 router.route("/getproofs").get(getProofRequests);
 router.route("/generateproof").post(generateProof);
+router.route("/addAdmin").post(registerAdmin);
 
 export default router;
